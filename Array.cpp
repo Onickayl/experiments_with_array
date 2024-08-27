d699328e481f0670ffe624aba15f2e9d27103507
Onickayl <elizabetafox@yandex.ru>
2024-08-27 16:30:48 +0300

    char array

--- Array.cpp
+++ Array.cpp
@@ -2,23 +2,23 @@
 #include <TXlib.h>
 #include <assert.h>
 
-void MoveData(int data[], size_t size);
+void MoveData(char data[], size_t size);
 
 int main()
 {
-    int data[] = {10, 20, 30, 40, 50};
+    char data[] = {'S', 'A', 'l', 'e', 'x', 'a', 'n', 'd', 'r', 'a'};
     size_t size = sizeof(data) / sizeof(*(data + 0));
     MoveData(data, size);
     return 0;
 }
 
-void MoveData(int* data, size_t size)
+void MoveData(char data[], size_t size)
 {
     for (size_t i = 0; i < size - 1; i++)
     {
         assert (i     < size);
         assert (i + 1 < size);
         *(data + i) = *(data + i + 1);
-        printf("data[%d] = %d\n", i, *(data + i));
+        printf("%c", data[i]);
     }
 }
