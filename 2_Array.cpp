b281d7beb9babc0ad4b31396ddb6379ea16e7d98
Onickayl <elizabetafox@yandex.ru>
2024-08-27 16:30:14 +0300

    replacement by *

--- 2_Array.cpp
+++ 2_Array.cpp
@@ -2,29 +2,35 @@
 #include <TXlib.h>
 #include <assert.h>
 
-void PrintData(int data[][3]);
+void PrintData(int* data, size_t sizeRow, size_t sizeSeat);
 
 int main()
 {
-    int data[4][3] = {{11, 12, 13}, {21, 22, 23},
-                  {31, 32, 33}, {41, 42, 43}};
+    int data[][4] = {{11, 12, 13, 5412},       // можно ли избавиться от числа?
+                    {21, 22, 23},
+                    {31, 32, 33},
+                    {41, 42, 43},
+                    {2847, 48924, 8725}};
 
-    //size_t size = sizeof(data) / sizeof(data[0]);
-    PrintData(data);
+    size_t sizeRow = sizeof(data) / sizeof(*(data + 0));
+    size_t sizeSeat = sizeof(*(data + 0)) / sizeof(*((int*)data + 0 + 0));
+    printf("sizeSeat = %d\n", sizeSeat);
+
+    PrintData(*data, sizeRow, sizeSeat);
     return 0;
 }
 
-void PrintData(int data[][3])
+void PrintData(int* data, size_t sizeRow, size_t sizeSeat)
 {
-    for (int row = 0; row < 4; row++)
+    for (size_t row = 0; row < sizeRow; row++)
     {
-        assert (row     < 5);
-        assert (row + 1 < 5);
-        for (int seat = 0; seat < 3; seat++)
+        //assert (row     < sizeRow);
+        //assert (row + 1 < sizeRow);
+        for (size_t seat = 0; seat < sizeSeat; seat++)
         {
-            assert (seat     < 4);
-            assert (seat + 1 < 4);
-            printf("data[%d][%d] = %d\n", row, seat, data[row][seat]);
+            //assert (seat     < sizeSeat);
+            //assert (seat + 1 < sizeSeat);
+            printf("data[%d][%d] = %d\n", row, seat, *((int*)data + row * sizeSeat + seat));
         }
         printf("\n");
 
